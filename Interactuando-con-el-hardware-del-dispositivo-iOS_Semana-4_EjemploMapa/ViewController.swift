eddcf66268f3077a3443489fdc1159358cb59df4
m0rb1u5 <goesrex@gmail.com>
2017-06-02 04:36:55 -0500

    Clase "Agregando mapas y anotaciones".

--- Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-4_EjemploMapa/ViewController.swift
+++ Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-4_EjemploMapa/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var mapa: MKMapView!
 
+    private let manejador = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.manejador.delegate = self
+        self.manejador.desiredAccuracy = kCLLocationAccuracyBest
+        self.manejador.requestWhenInUseAuthorization()
+        
+        var punto = CLLocationCoordinate2D()
+        punto.latitude = 19.52748
+        punto.longitude = -96.92315
+        let pin = MKPointAnnotation()
+        pin.title = "Xalapa"
+        pin.subtitle = "Veracruz"
+        pin.coordinate = punto
+        mapa.addAnnotation(pin)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            self.manejador.startUpdatingLocation()
+            self.mapa.showsUserLocation = true
+        }
+        else {
+            self.manejador.stopUpdatingLocation()
+            self.mapa.showsUserLocation = false
+        }
+    }
 }
 
